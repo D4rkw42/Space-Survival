6533654120c06a33b33c585561777835838173b6
William da Conceição Pereira <williamcpdw014@gmail.com>
2020-11-12 14:32:47 -0300

    Description
    The game recources.
    
    @Edit: cleaned unecessary lines.

--- Space Survival/src/classes/Ship.js
+++ Space Survival/src/classes/Ship.js
@@ -2,8 +2,6 @@ class Ship {
     constructor(x, y, ship) {
         this.x= x;
         this.y= y;
-        this.lastX= this.x;
-        this.lastY= this.y;
         this.ship= document.getElementById(ship);
         this.maxLife= 300;
         this.life= this.maxLife;
@@ -11,7 +9,5 @@ class Ship {
     draw() {
         if (this.life> 0) context.drawImage(this.ship, this.x, this.y);
         context.fillStyle="yellow";
-        this.lastX= this.x;
-        this.lastY= this.y;
     };
 };
